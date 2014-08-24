da7e5149f442dfacbf554005966f91f7d4a63095
redacted-moose <sadlercr@rose-hulman.edu>
2014-08-23 21:09:48 -0500

    File ignores, and fix cpu structure to actually store registers correctly

--- cpu.h
+++ cpu.h
@@ -31,25 +31,25 @@ typedef struct cpu_t {
 	// General purpose / Flags
 	union {
 		struct {
-			byte a : 8;
-//			byte f;
-			byte z : 1;
-			byte n : 1;
-			byte hc : 1;
-			byte ca : 1;
 			byte   : 4;
+			byte ca : 1;
+			byte hc : 1;
+			byte n : 1;
+			byte z : 1;
+//			byte f;
+			byte a : 8;
 		};
 
 		struct {
-			byte a7 : 1;
-			byte a6 : 1;
-			byte a5 : 1;
-			byte a4 : 1;
-			byte a3 : 1;
-			byte a2 : 1;
-			byte a1 : 1;
-			byte a0 : 1;
 			byte    : 8;
+			byte a0 : 1;
+			byte a1 : 1;
+			byte a2 : 1;
+			byte a3 : 1;
+			byte a4 : 1;
+			byte a5 : 1;
+			byte a6 : 1;
+			byte a7 : 1;
 		};
 
 		word af;
@@ -57,27 +57,28 @@ typedef struct cpu_t {
 
 	union {
 		struct {
-			byte b;
 			byte c;
+			byte b;
 		};
 
 		struct {
-			byte b7 : 1;
-			byte b6 : 1;
-			byte b5 : 1;
-			byte b4 : 1;
-			byte b3 : 1;
-			byte b2 : 1;
-			byte b1 : 1;
-			byte b0 : 1;
-			byte c7 : 1;
-			byte c6 : 1;
-			byte c5 : 1;
-			byte c4 : 1;
-			byte c3 : 1;
-			byte c2 : 1;
-			byte c1 : 1;
 			byte c0 : 1;
+			byte c1 : 1;
+			byte c2 : 1;
+			byte c3 : 1;
+			byte c4 : 1;
+			byte c5 : 1;
+			byte c6 : 1;
+			byte c7 : 1;
+
+			byte b0 : 1;
+			byte b1 : 1;
+			byte b2 : 1;
+			byte b3 : 1;
+			byte b4 : 1;
+			byte b5 : 1;
+			byte b6 : 1;
+			byte b7 : 1;
 		};
 
 		word bc;
@@ -85,27 +86,28 @@ typedef struct cpu_t {
 
 	union {
 		struct {
-			byte d;
 			byte e;
+			byte d;
 		};
 
 		struct {
-			byte d7 : 1;
-			byte d6 : 1;
-			byte d5 : 1;
-			byte d4 : 1;
-			byte d3 : 1;
-			byte d2 : 1;
-			byte d1 : 1;
-			byte d0 : 1;
-			byte e7 : 1;
-			byte e6 : 1;
-			byte e5 : 1;
-			byte e4 : 1;
-			byte e3 : 1;
-			byte e2 : 1;
-			byte e1 : 1;
 			byte e0 : 1;
+			byte e1 : 1;
+			byte e2 : 1;
+			byte e3 : 1;
+			byte e4 : 1;
+			byte e5 : 1;
+			byte e6 : 1;
+			byte e7 : 1;
+
+			byte d0 : 1;
+			byte d1 : 1;
+			byte d2 : 1;
+			byte d3 : 1;
+			byte d4 : 1;
+			byte d5 : 1;
+			byte d6 : 1;
+			byte d7 : 1;
 		};
 
 		word de;
@@ -113,27 +115,28 @@ typedef struct cpu_t {
 
 	union {
 		struct {
-			byte h;
 			byte l;
+			byte h;
 		};
 
 		struct {
-			byte h7 : 1;
-			byte h6 : 1;
-			byte h5 : 1;
-			byte h4 : 1;
-			byte h3 : 1;
-			byte h2 : 1;
-			byte h1 : 1;
-			byte h0 : 1;
-			byte l7 : 1;
-			byte l6 : 1;
-			byte l5 : 1;
-			byte l4 : 1;
-			byte l3 : 1;
-			byte l2 : 1;
-			byte l1 : 1;
 			byte l0 : 1;
+			byte l1 : 1;
+			byte l2 : 1;
+			byte l3 : 1;
+			byte l4 : 1;
+			byte l5 : 1;
+			byte l6 : 1;
+			byte l7 : 1;
+
+			byte h0 : 1;
+			byte h1 : 1;
+			byte h2 : 1;
+			byte h3 : 1;
+			byte h4 : 1;
+			byte h5 : 1;
+			byte h6 : 1;
+			byte h7 : 1;
 		};
 
 		word hl;
